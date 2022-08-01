e7554a05cb0503bf804258fc7db21fdd0f8ec8d0
judeno <judeno@gmail.com>
2022-08-01 18:49:01 +0900

    first commit

---
+++ js/configFirebase.js
@@ -0,0 +1,9 @@
+export default {
+	apiKey: "AIzaSyCU0i7vjxag1PiT0MGkW4hbe_Gd2XHHRu4",
+	authDomain: "pd-egame-stage.firebaseapp.com",
+	projectId: "pd-egame-stage",
+	storageBucket: "pd-egame-stage.appspot.com",
+	messagingSenderId: "556851501342",
+	appId: "1:556851501342:web:a6039270e67f20e713110f",
+	measurementId: "G-BEH99SB7G4"
+}
